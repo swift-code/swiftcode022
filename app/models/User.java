2caa7818700a33797066d9733f364170868802bd
Live session user <sandeep.k@betsol.com>
2016-08-20 11:39:02 +0000

    forms completed

--- app/models/User.java
+++ app/models/User.java
@@ -1,6 +1,7 @@
 package models;
 
 import com.avaje.ebean.Model;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.persistence.*;
 import java.util.List;
@@ -37,5 +38,15 @@ public class User extends Model{
     )
 
     public Set<User> connection;
+    public static User authenticate(String email, String password)
+    {
+        User user= User.find.where().eq("email",email).findUnique();
+        if(user != null && BCrypt.checkpw(password,user.password))
+        {
+            return user;
 
+        }
+        return null;
+    }
+public static Finder<Long,User> find = new Finder<Long,User>(User.class);
 }
